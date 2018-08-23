31a187e2943d5a753099a940e9118021e9cbef58
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2018-08-22 18:59:42 -0600

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Update
+sudo apt-get update
+sudo apt-get upgrade
+
+# C++
+sudo apt-get install -y clang gcc g++ libboost-all-dev 
+
+# MySQL
+sudo apt-get install -y mysql-server mysql-client
+
+#Java
+sudo apt-get install -y default-jdk* gradle maven openjdk-8-jdk openjfx libgit2-dev
+
+# Python3
+sudo apt-get install -y python3-dev python3-pip python3-setuptools
