64a61393d3b9f73c2367d9617a4bd41b81722f72
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2018-08-28 14:57:51 -0600

    Add code comments to code_quality.py

--- merge_excavator/code_quality.py
+++ merge_excavator/code_quality.py
@@ -14,16 +14,15 @@ def get_commit_quality(repository_name, commit, operation):
     :return: 1 if the code can compile or pass the tests, 0 otherwise
     """
     cd_to_repository = 'cd {};'.format(config.REPOSITORY_PATH + repository_name)
-    if commit != -1:
+    if commit != -1: # Valid commit SHA-1
         os.system(cd_to_repository + 'git checkout {}'.format(commit))
-    repository_dir = config.REPOSITORY_PATH + repository_name
-    if os.path.isfile(repository_dir + '/pom.xml'): # Maven
+    if os.path.isfile('{}.pom.xml'.format(config.REPOSITORY_PATH + repository_name)): # Maven
         if operation == 'compile':
             command = 'compile test-compile'
         elif operation == 'test':
             command = 'test'
         else:
-            raise ValueError('Operator {} is not defined'.format(operation))
+            raise ValueError('Operator {} is not defined for {}'.format(operation, repository_name))
         maven_output = os.popen(cd_to_repository + 'mvn ' + command).read()
         os.system(cd_to_repository + 'git reset --hard'.format(commit))
         if maven_output.find('BUILD SUCCESS') > -1 or maven_output.find('BUILD FAILURE') == -1:
@@ -33,10 +32,16 @@ def get_commit_quality(repository_name, commit, operation):
         else:
             raise ValueError('Cannot determine the compile/test status of {}'.format(repository_name))
     else:
-        raise ValueError('This tool does not support {} build system'.format(repository_name))
+        raise ValueError('This tool does not support {}\'s build system'.format(repository_name))
 
 
 def get_code_violation_num(repository_name, commit):
+    """
+    Returns the number of code violations by StyleChecker using Google rules
+    :param repository_name:   The name of the repository in <USER_NAME>/<REPOSITORY_NAME> format
+    :param commit: The commit to checkout
+    :return: The number of code violations
+    """
     repository_dir = config.REPOSITORY_PATH + repository_name.replace('/', '___')
     cd_to_repository = 'cd {};'.format(repository_dir)
     os.system(cd_to_repository + 'git checkout --quiet {}'.format(commit))
@@ -45,16 +50,29 @@ def get_code_violation_num(repository_name, commit):
 
 
 def get_code_complexity_of_dir(repository_dir):
+    """
+    Returns the code complexity of the code in a directory using Lizard tool.
+    :param repository_dir:   The directory of the code to analyze
+    :return: A vector with size of eight as the code complexity
+    """
     cd_to_repository = 'cd {};'.format(repository_dir)
     commits = os.popen(cd_to_repository + 'lizard -C 1000').readlines()[-1].split()
     return commits
 
 
-def get_code_complexity_diff(repository_name, commit1, commit2):
+def get_code_complexity_diff(repository_name, commit_1, commit_2):
+    """
+    Returns the code complexity difference between two commits using norm-1 operator.
+    :param repository_name:   The name of the repository in <USER_NAME>/<REPOSITORY_NAME> format
+    :param commit_1: The first commit commit to analyze
+    :param commit_2: The second commit commit to analyze
+
+    :return: A vector with size of eight as the code complexity
+    """
     repository_dir = config.REPOSITORY_PATH + repository_name.replace('/', '___')
     cd_to_repository = 'cd {};'.format(repository_dir)
-    os.system(cd_to_repository + 'git checkout --quiet {}'.format(commit1))
+    os.system(cd_to_repository + 'git checkout --quiet {}'.format(commit_1))
     complexity1 = get_code_complexity_of_dir(repository_dir)
-    os.system(cd_to_repository + 'git checkout --quiet {}'.format(commit2))
+    os.system(cd_to_repository + 'git checkout --quiet {}'.format(commit_2))
     complexity2 = get_code_complexity_of_dir(repository_dir)
     return np.asarray(complexity1, dtype=float) - np.asarray(complexity2, dtype=float)
