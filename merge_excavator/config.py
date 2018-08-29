7a4d5e29589a6c430c2a82c1dcd0489ba641afbf
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2018-08-28 18:10:09 -0600

    Add config.py

---
+++ merge_excavator/config.py
@@ -0,0 +1,16 @@
+
+# Keys
+GITHUB_KEY = ''
+
+# Paths
+REPOSITORY_PATH = '../working_dir/repository/'
+TEMP_CSV_PATH = '../working_dir/csv_files/'
+REPOSITORY_LIST_PATH = '../working_dir/repository_lists/'
+LOG_PATH = '../working_dir/logs/'
+QUERY_PATH = '../queries/'
+
+# DB information
+DB_HOST = ''
+DB_NAME = ''
+DB_USER_NAME = ''
+DB_PASSWORD = ''
