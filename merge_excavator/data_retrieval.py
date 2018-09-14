ef3c03b9c45464c0ad141d0f3c2adaf995854f86
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2018-09-14 16:40:36 -0600

    Add more data analysis queries

--- merge_excavator/data_retrieval.py
+++ merge_excavator/data_retrieval.py
@@ -60,6 +60,41 @@ class Data_Retreival:
                                'FROM Merge_Data.Merge_Scenario Merge_Scenario'
         self.is_conflict_query = 'SELECT Merge_Replay.Merge_Scenario_merge_commit_hash, Merge_Replay.is_conflict ' \
                            'FROM Merge_Data.Merge_Replay Merge_Replay'
+        self.conflict_rate_query = """SELECT scenarios.name AS 'Repository Name'
+                                    , scenarios.scenarios AS '# Merge Scenarios', 
+                                    conflicts.conflicts AS '# Merge Scenarios with Conflicts', 
+                                    100 * conflicts.conflicts/scenarios.scenarios 'Conflict Rate (%)'
+                                    FROM
+                                    (SELECT Repository.name, 
+                                    COUNT(Merge_Replay.Merge_Scenario_merge_commit_hash) AS 'scenarios'
+                                    FROM  Merge_Data.Repository  
+                                    JOIN  Merge_Data.Merge_Replay 
+                                    ON id = Merge_Scenario_Repository_id
+                                    GROUP BY name
+                                    ORDER BY name) scenarios
+                                    INNER JOIN
+                                    (SELECT name, COUNT(Merge_Replay.Merge_Scenario_merge_commit_hash)  AS 'conflicts'
+                                    FROM  Merge_Data.Repository
+                                    JOIN  Merge_Data.Merge_Replay
+                                    ON id = Merge_Scenario_Repository_id
+                                    WHERE is_conflict = 1
+                                    GROUP BY name
+                                    ORDER BY name) conflicts 
+                                    ON scenarios. name = conflicts.name"""
+        self.repository_stat = """(SELECT MIN(star_num) as Min, AVG(star_num) as AVG,MAX(star_num) as Max
+                                FROM Merge_Data.Repository)
+                                UNION ALL
+                                (SELECT MIN(watch_num), AVG(watch_num),MAX(watch_num)
+                                FROM Merge_Data.Repository)
+                                UNION ALL
+                                (SELECT MIN(fork_num), AVG(fork_num),MAX(fork_num)
+                                FROM Merge_Data.Repository)
+                                UNION ALL
+                                (SELECT MIN(issue_num), AVG(issue_num),MAX(issue_num)
+                                FROM Merge_Data.Repository)
+                                UNION ALL
+                                (SELECT MIN(size) / 1024, AVG(size) / 1024,MAX(size) / 1024
+                                FROM Merge_Data.Repository)"""
 
     def get_query_result(self, query):
         return os.popen('mysql -u {} -e "{}"'.format(config.DB_USER_NAME, query)).read()
@@ -148,6 +183,12 @@ class Data_Retreival:
             .to_csv(path_or_buf=config.PREDICTION_CSV_PATH + config.PREDICTION_CSV_DATA_NAME)
         self.get_is_conflict().to_csv(path_or_buf=config.PREDICTION_CSV_PATH + config.PREDICTION_CSV_LABEL_NAME)
 
+    def get_conflict_ratio(self):
+        return self.get_data_frame_of_query_result(self.get_query_result(self.conflict_rate_query))
+
+    def get_repository_stats(self):
+        return self.get_data_frame_of_query_result(self.get_query_result(self.repository_stat)).rename(index={0:'star', 1:'watch', 2: 'fork', 3: 'issue', 4:'size'})
+
     def print_df_stats(self, df):
         print('DataFrame Stats:')
         print('  - # Data Points: {}'.format(df.shape[0]))
@@ -155,5 +196,7 @@ class Data_Retreival:
         print('  - Index: {}'.format(df.index))
         print('  - Columns: {}'.format(df.columns))
 
+
 obj = Data_Retreival()
-obj.save_prediction_data_to_csv()
+#obj.save_prediction_data_to_csv()
+print(obj.get_repository_stats())
