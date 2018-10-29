6d7fa7053b3426fb45a5144c81aa4f3ed4c97f25
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2018-10-29 13:35:40 -0600

    Refactor the repository_data.py

--- merge_excavator/repository_data.py
+++ merge_excavator/repository_data.py
@@ -10,20 +10,25 @@ import validation
 
 def store_repository_info(repository_name, merge_scenario_num, is_done):
     """
-    This method stores the information of the given repository from GitHub. TEMP_CSV_PATH should be set in config.py
-    :param
-    repository_url: The name of the repository in <USER_NAME>/<REPOSITORY_NAME> format
-    :return: The Nothing
+    Stores the information of the given repository from GitHub. TEMP_CSV_PATH should be set in config.py
+    :param repository_name:  The name of the repository in <USER_NAME>/<REPOSITORY_NAME> format
+    :param merge_scenario_num: The number of merge scenarios
+    :param is_done: whether the analyzing of the repository is done
+    :return: The id of the given repository
     """
     validation.validate_repository_name(repository_name)
 
     github_request = os.popen('curl --silent -H "Authorization: token  ' + config.GITHUB_KEY + \
                                '"  https://api.github.com/repos/' + repository_name.replace('___', '/')).read()
+
+    # Check if the repository was available
     json_data = json.loads(github_request)
     if 'message' in json_data.keys() and json_data['message'] != 'Moved Permanently':
-         raise ValueError('The repository {} does not exists.'.format(repository_name))
+         raise ValueError('The repository {} permanently moved.'.format(repository_name))
     if 'message' in json_data.keys() and json_data['message'] == 'Moved Permanently':
-         return -1
+        raise ValueError('The repository {} is unavailable.'.format(repository_name))
+
+    # Store the data
     current_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     repository_data = [json_data['id'], current_time, json_data['full_name'], json_data['description'], \
                       json_data['language'], json_data['subscribers_count'], \
@@ -33,6 +38,7 @@ def store_repository_info(repository_name, merge_scenario_num, is_done):
     csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"')
     csv_writer.writerow(repository_data)
     csv_file.close()
+
     return json_data['id']
 
 
@@ -42,15 +48,19 @@ def get_repository_id(repository_name):
     This method stores the information of the given repository from GitHub. TEMP_CSV_PATH should be set in config.py
     :param
     repository_url: The name of the repository in <USER_NAME>/<REPOSITORY_NAME> format
-    :return: The Nothing
+    :return: The id and size of the given repository
     """
     validation.validate_repository_name(repository_name)
 
     github_request = os.popen('curl --silent -H "Authorization: token  ' + config.GITHUB_KEY + \
                                '"  https://api.github.com/repos/' + repository_name.replace('___', '/')).read()
     json_data = json.loads(github_request)
+
+    # Check if the repository was available
+    json_data = json.loads(github_request)
     if 'message' in json_data.keys() and json_data['message'] != 'Moved Permanently':
-         raise ValueError('The repository {} does not exists.'.format(repository_name))
+         raise ValueError('The repository {} permanently moved.'.format(repository_name))
     if 'message' in json_data.keys() and json_data['message'] == 'Moved Permanently':
-         return -1
+        raise ValueError('The repository {} is unavailable.'.format(repository_name))
+
     return json_data['id'], json_data['size']
