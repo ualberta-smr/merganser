1c8b632ee326a87052139faa73b79201e5c329c9
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2018-10-29 12:57:01 -0600

    Edit the comments of util.py

--- merge_excavator/util.py
+++ merge_excavator/util.py
@@ -1,22 +1,22 @@
-import os
 
+import os
 import config
+
 
 def remove_remopitory(repository_name):
     """
-    Remove the temp repository directory
-    :return:
+    Clean up the repository directory
+    :param repository_name:  The name of the repository in <USER_NAME>/<REPOSITORY_NAME> format
+    :return: -
     """
     os.system('rm -r -f {}'.format(os.getcwd() + '/' + config.REPOSITORY_PATH + repository_name))
-
 
 
 def remove_dir():
     """
     Remove the temp directories in config.py
-    :return:
+    :return: -
     """
-    # os.system('rm -r -f {}'.format(config.REPOSITORY_PATH))
     os.system('rm -r -f {}'.format(config.TEMP_CSV_PATH))
     os.system('rm -r -f {}'.format(config.LOG_PATH))
     os.system('rm -r -f {}'.format(config.PREDICTION_CSV_PATH))
@@ -25,7 +25,7 @@ def remove_dir():
 def create_dir():
     """
     create the temp directories in config.py
-    :return:
+    :return: -
     """
     os.system('mkdir {}'.format(config.REPOSITORY_PATH))
     os.system('mkdir {}'.format(config.TEMP_CSV_PATH))
