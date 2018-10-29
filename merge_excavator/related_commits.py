b01bc89ee84c66339f53efb07319ebc876e49572
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2018-10-29 15:01:34 -0600

    Add documentations to related_commits.py

--- merge_excavator/related_commits.py
+++ merge_excavator/related_commits.py
@@ -2,26 +2,41 @@
 # -*- coding: utf-8 -*-
 
 import csv
-import numpy as np
 
 from config import *
 from GitUtil import *
 
+
 def store_commit_info_between_two_commits(git_utility, commit1, commit2, parent_num, merge_commit, repository_id):
-    commit_list = git_utility.get_commit_list_between_two_commits(commit1, commit2) # The first commit (index zero)
+    """
+    Store the information of commits that are between two given commits A and B
+    :param git_utility: The GitUtil object
+    :param commit1: Commit A
+    :param commit2: Commit B
+    :param parent_num: The parent number. Instead of using the branch name, the number of branches is used
+    :param merge_commit: Merge commit SHA-1
+    :param repository_id: The id of the repository
+    """
+
+    # Extract the list of commits
+    commit_list = git_utility.get_commit_list_between_two_commits(commit1, commit2)
+
     for commit in commit_list:
         commit = commit.strip()
+
+        # Do not consider the head and the tail
         if commit == merge_commit or commit == commit1:
             continue
+
         commit_date  = git_utility.get_commit_date(commit)
-        commit_message = git_utility.get_commit_message(commit)[0:300]
+        commit_message = git_utility.get_commit_message(commit)[0:500]
+
+        # Empty commit messages
         if len(commit_message) == 0:
             commit_message = 'null'
         branch_name = git_utility.get_branch_of_commit(commit)
         file_changes = git_utility.get_changed_files_in_commit(commit)
         line_changes = git_utility.get_changed_lines_in_commit(commit)
-
-        branch_name = 'TEMP'
 
         # Store the merge related commits
         merge_related_commits_data = [commit.strip(), commit_date, commit_message, branch_name, parent_num] + \
