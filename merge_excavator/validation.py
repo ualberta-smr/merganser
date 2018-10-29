5ec177b5f79e1790eff8296027344bef94aea35e
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2018-10-29 12:56:40 -0600

    Edit the comments of validation.py

--- merge_excavator/validation.py
+++ merge_excavator/validation.py
@@ -6,7 +6,6 @@ def validate_repository_name(repository_name):
     """
     Check if repository_name follows the right format.
     :param repository_name: The name of the repository in <USER_NAME>/<REPOSITORY_NAME> format
-    :return:
     """
     if repository_name.count('___') + repository_name.count('/') != 1:
         raise ValueError('The repository name should be in <USER_NAME>/<REPOSITORY_NAME> format. {} is invalid.'.
@@ -18,7 +17,6 @@ def validate_core_num(core_num):
     Check if the code_num follows the right format. This variable should be an integer between one and the number of
     cores on the machine.
     :param core_num: The maximum number of cores to use in processing
-    :return:
     """
     if core_num < 1:
         raise ValueError('The number of cores should be greater than zero. {} is not a valid value.'.format(core_num))
