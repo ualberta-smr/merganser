3bc68dccbbaa02e8b835d4ed29a3fc96a419a293
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2018-10-31 19:51:06 -0600

    Update the documentations of clone_repository.py

--- merge_excavator/clone_repositories.py
+++ merge_excavator/clone_repositories.py
@@ -12,7 +12,6 @@ def clone_repository(repository_name):
     Receiving a name of the repository in  <USER_NAME>/<REPOSITORY_NAME> format, this method clone the repository from
     GitHub in REPOSITORY_PATH which is set in config.py
     :param repository_name:   The name of the repository in <USER_NAME>/<REPOSITORY_NAME> format
-    :return: Nothing
     """
     validation.validate_repository_name(repository_name)
     cd_to_repository = 'cd {};'.format(config.REPOSITORY_PATH)
@@ -28,7 +27,6 @@ def clone_repositories(repository_list, core_num = multiprocessing.cpu_count()):
      REPOSITORY_LIST_PATH which is set in config.txt
     :param repository_list:    The name of the repository in <USER_NAME>/<REPOSITORY_NAME> format
     :param core_num: The number of parallel threads. Default is all of the available cores
-    :return: Nothing
     """
     validation.validate_core_num(core_num)
     repositories = open(config.REPOSITORY_LIST_PATH + repository_list + '.txt', 'rt').readlines()
