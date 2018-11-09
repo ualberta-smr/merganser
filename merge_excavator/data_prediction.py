45bb08eaaf5100a74f6ed0230e470195036890d4
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2018-11-09 15:51:01 -0700

    Update the data preparation for prediction

--- merge_excavator/data_prediction.py
+++ merge_excavator/data_prediction.py
@@ -65,19 +65,18 @@ class Data_Retreival:
         """
 
         self.commit_message_quey = """
-        SELECT Merge_Scenario.merge_commit_hash as 'merge_commit', GROUP_CONCAT(Commits.message SEPARATOR \' ||| \') 
-        FROM Merge_Data.Merge_Scenario merge_scenario 
-        LEFT JOIN Merge_Data.Merge_Related_Commit Commits 
-        on merge_scenario.merge_commit_hash = Commits.Merge_Scenario_merge_commit_hash 
-        AND Commits.merge_commit_parent = {}
-        GROUP BY merge_scenario.merge_commit_hash
+        SELECT merge_commit_hash as 'merge_commit', LOWER(GROUP_CONCAT(message SEPARATOR ' ||| ')) as 'commit_messages'
+        FROM Merge_Data.Merge_Scenario 
+        JOIN Merge_Data.Merge_Related_Commit  
+        on merge_commit_hash = Merge_Scenario_merge_commit_hash 
+        where parallel_changed_file_num > 0
+        GROUP BY merge_commit_hash;
        """
 
         self.is_conflict_query = """
         SELECT  
         merge_scenario.merge_commit_hash as 'merge_commit',
-        
-        merge_replay.is_conflict
+        merge_replay.is_conflict as 'is_conflict'
         
         FROM Merge_Data.Merge_Scenario as merge_scenario 
         JOIN Merge_Data.Merge_Replay as merge_replay ON  merge_scenario.merge_commit_hash = merge_replay.Merge_Scenario_merge_commit_hash
@@ -96,22 +95,21 @@ class Data_Retreival:
         query_result = os.popen('mysql -u {} -e "{}"'.format(config.DB_USER_NAME, query)).read()
         return pd.read_csv(StringIO(query_result), delimiter='\t')
 
-    def get_merge_scenario_prediction_data(self):
-        data = self.get_light_features()
-
-        label = self.get_query_result(self.is_conflict_query.format())
-
-        label = label.drop('merge_commit', axis=1)
+    def get_merge_scenario_prediction_data(self, post_name):
+        logging.info('Preparing data and label of prediction task for {}'.format(post_name.replace('_', '')))
+        messages_features = self.get_commit_messege_characteristics()
+        data = pd.concat([self.get_light_features(), self.get_commit_density(), messages_features[0], messages_features[1]], axis=1).drop('merge_commit', axis=1)
+        label = self.get_query_result(self.is_conflict_query.format()).drop('merge_commit', axis=1)
         return data, label
 
     def save_prediction_data(self, post_name):
-        logging.info('Start {}'.format(post_name))
-        data, label = self.get_merge_scenario_prediction_data()
+        logging.info('Saving data and label of prediction task for {}'.format(post_name.replace('_', '')))
+        data, label = self.get_merge_scenario_prediction_data(post_name)
         data.to_csv(path_or_buf=config.PREDICTION_CSV_PATH + config.PREDICTION_CSV_DATA_NAME + post_name)
         label.to_csv(path_or_buf=config.PREDICTION_CSV_PATH + config.PREDICTION_CSV_LABEL_NAME + post_name)
 
     def get_light_features(self):
-        logging.info('Extracting code complexity...')
+        logging.info('Extracting light-weight features...')
         return self.get_query_result(self.light_features_query)
 
     def get_commit_density(self):
@@ -121,40 +119,25 @@ class Data_Retreival:
         density_diff =  density_paren2['commit_density'] - density_paren1['commit_density']
         return pd.concat([density_paren1['merge_commit'], density_diff], axis=1)
 
-    def get_commit_messege_characteristics(self, parent):
+    def get_commit_messege_characteristics(self):
         logging.info('Extracting message characteristics...')
-        commit_messages = self.get_query_result(self.commit_message_quey.format(parent))
-        commit_messages_list = commit_messages.values[1:]
-        commit_messages_list = [item for sublist in commit_messages_list for item in sublist if not isNaN(item)]
-        print((commit_messages_list))
-        exit()
+        commit_messages = self.get_query_result(self.commit_message_quey.format())
         keywords = sorted(['fix', 'bug', 'feature', 'improve', 'document', 'refactor', 'update', 'add', 'remove', 'use',
                         'delete', 'change'])
-        keywords_frequency = []
-        commit_messege_length_stats = []
-        for merge_scenrio_commits in commit_messages_list:
-            print(commit_messages_list)
-            keywords_frequency.append([word.lower().count(word) for word in keywords])
-            print(keywords_frequency)
-            exit()
-            if merge_scenrio_commits != 'NULL':
-                seperated_commit_message = merge_scenrio_commits.replace(' ||| ', '\n').split('\n')
-                commit_messege_length = [len(msg.split()) for msg in seperated_commit_message]
-                commit_messege_length_stats.append([np.min(commit_messege_length), np.mean(commit_messege_length),
-                                               np.median(commit_messege_length), np.max(commit_messege_length)])
-            else:
-                commit_messege_length_stats.append([0.0, 0.0, 0.0, 0.0])
-        column_names_frequency = ['# fix', '# bug', '# feature', '# improve', '# document', '# refactor', '# update',
-                                  '# add', '# remove', '# use', '# delete', '# change']
-        column_names_stats = ['Min Msg Length', 'Mean Msg Length', 'Median Msg Length', 'Max Msg Length']
-        return pd.DataFrame(keywords_frequency, columns=column_names_frequency), \
-               pd.DataFrame(commit_messege_length_stats, columns=column_names_stats)
+        keywords_names = [keyword + '_frequency' for keyword in keywords]
+        keywords_frequency = np.zeros((commit_messages.shape[0], len(keywords)))
+        messages_stats = np.zeros((commit_messages.shape[0], 4))
+        for index, merge in commit_messages.iterrows():
+            for keyword_id, keyword in enumerate(keywords):
+                keywords_frequency[index, keyword_id] = merge['commit_messages'].count(keyword)
+            messagess_len = [len(message) for message in merge['commit_messages'].split('|||')]
+            messages_stats[index, :] = [np.min(messagess_len), np.max(messagess_len),
+                                        np.mean(messagess_len), np.median(messagess_len)]
+        keywords_frequency_df = pd.DataFrame(data=keywords_frequency, columns=keywords_names)
+        messages_stats_df =  pd.DataFrame(data=messages_stats, columns=['messages_min', 'messages_max',
+                                                                        'messages_mean', 'messages_median'])
+        return keywords_frequency_df, messages_stats_df
 
-    def get_commit_message_features(self):
-        keywords_frequency1, commit_messege_length_stats1 = self.get_commit_messege_characteristics(1)
-        keywords_frequency2, commit_messege_length_stats2 = self.get_commit_messege_characteristics(2)
-        keywords_frequency2 - keywords_frequency1,
-        commit_messege_length_stats2 - commit_messege_length_stats1
 
 if __name__ == "__main__":
 
@@ -163,7 +146,7 @@ if __name__ == "__main__":
                         format='%(levelname)s in %(threadName)s - %(asctime)s by %(name)-12s :  %(message)s',
                         datefmt='%y-%m-%d %H:%M:%S')
     obj = Data_Retreival()
-    data_df = obj.save_prediction_data('_all')
+    data_df = obj.save_prediction_data('_All2')
     #print(data_df)
 
 
