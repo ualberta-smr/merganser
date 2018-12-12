f22dc23666f5dbcdc4e9bb922d0bfcf89645c8a2
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2018-12-12 16:14:33 -0700

    Style changes in the tests

--- tests/test_run_time.py
+++ tests/test_run_time.py
@@ -20,28 +20,32 @@ class MyTest(unittest.TestCase):
 
     def test_check_repository_csv_file(self):
         os.system('./runPredict.sh samplemerge')
-        df = pd.read_csv(config.TEMP_CSV_PATH.replace('..', '.') + 'Repository_owhadi___sample-merge.csv', header=None)
+        df = pd.read_csv(config.TEMP_CSV_PATH.replace('..', '.') + 'Repository_owhadi___sample-merge.csv',
+                         header=None)
         self.assertEqual(df.values[0][0], 156289982)
         self.assertEqual(df.values[0][-1], 1)
         self.assertEqual(df.values[0][-2], 2)
 
     def test_check_merge_scenario_csv_file(self):
         os.system('./runPredict.sh samplemerge')
-        df = pd.read_csv(config.TEMP_CSV_PATH.replace('..', '.') + 'Merge_Scenario_owhadi___sample-merge.csv', header=None)
+        df = pd.read_csv(config.TEMP_CSV_PATH.replace('..', '.') + 'Merge_Scenario_owhadi___sample-merge.csv',
+                         header=None)
         self.assertEqual(df.values[0][0], '18a168ea05b242d61f51cf1b51ac98feb205e337')
         self.assertEqual(df.values[0][-1], 156289982)
         self.assertEqual(df.values[0][-2], 0)
 
     def test_check_merge_replay_csv_file(self):
         os.system('./runPredict.sh samplemerge')
-        df = pd.read_csv(config.TEMP_CSV_PATH.replace('..', '.') + 'Merge_Replay_owhadi___sample-merge.csv', header=None)
+        df = pd.read_csv(config.TEMP_CSV_PATH.replace('..', '.') + 'Merge_Replay_owhadi___sample-merge.csv',
+                         header=None)
         self.assertEqual(df.values[0][1], 1)
         self.assertEqual(df.values[0][-1], 156289982)
         self.assertEqual(df.values[0][-2], '18a168ea05b242d61f51cf1b51ac98feb205e337')
 
     def test_check_merge_related_commit_csv_file(self):
         os.system('./runPredict.sh samplemerge')
-        df = pd.read_csv(config.TEMP_CSV_PATH.replace('..', '.') + 'Merge_Related_Commit_owhadi___sample-merge.csv', header=None)
+        df = pd.read_csv(config.TEMP_CSV_PATH.replace('..', '.') + 'Merge_Related_Commit_owhadi___sample-merge.csv',
+                         header=None)
         self.assertEqual(df.values[1][0], '3619eb024f00aa9d4e753df4d76784ec0209315a')
         self.assertEqual(df.values[1][-1], 156289982)
         self.assertEqual(df.values[1][-2], '18a168ea05b242d61f51cf1b51ac98feb205e337')
