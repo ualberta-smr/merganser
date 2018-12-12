22211ad17508c52ef3f328d1a4d2e0a2623d9bd3
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2018-12-12 12:32:11 -0700

    Add the data insertion code

---
+++ merganser/data_conversion.py
@@ -0,0 +1,28 @@
+
+import os
+
+import config
+
+cd_to_csv = 'cd {};'.format(config.TEMP_CSV_PATH)
+table_list = ['Repository',
+            'Conflicting_File',
+            'Conflicting_Region',
+            'Merge_Scenario',
+            'Merge_Replay',
+            'Code_Style_Violation',
+            'Code_Complexity',
+            'Merge_Related_Commit']
+
+# Create tables
+os.system('mysql -u {} --password={} < {}Merge_Data.sql'.format(config.DB_USER_NAME
+                                                                , config.DB_PASSWORD, config.QUERY_PATH))
+
+# Insert the data
+os.system(cd_to_csv + 'mkdir temp')
+for table in table_list:
+    os.system(cd_to_csv + 'cat {}_*  | tr -d "\r" > ./temp/{}.csv'.format(table, table))
+    os.system(cd_to_csv + 'mysqlimport --fields-escaped-by=\'\\\' --fields-optionally-enclosed-by=\'\"\'  '
+                          '--fields-terminated-by="," --lines-terminated-by="\n"  '
+                          '--verbose  --local -u {} --password={} Merge_Data  ./temp/{}.csv '
+              .format(config.DB_USER_NAME, config.DB_PASSWORD, table))
+os.system(cd_to_csv + 'rm -r temp')
