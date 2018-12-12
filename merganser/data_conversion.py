23fc3276aec64be72c8982b3e1cd015d44cfe169
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2018-12-12 12:35:32 -0700

    Add documentation to data_conversion.py

--- merganser/data_conversion.py
+++ merganser/data_conversion.py
@@ -3,26 +3,36 @@ import os
 
 import config
 
-cd_to_csv = 'cd {};'.format(config.TEMP_CSV_PATH)
-table_list = ['Repository',
-            'Conflicting_File',
-            'Conflicting_Region',
-            'Merge_Scenario',
-            'Merge_Replay',
-            'Code_Style_Violation',
-            'Code_Complexity',
-            'Merge_Related_Commit']
 
-# Create tables
-os.system('mysql -u {} --password={} < {}Merge_Data.sql'.format(config.DB_USER_NAME
-                                                                , config.DB_PASSWORD, config.QUERY_PATH))
+def insert_csv_into_mysql():
+    """
+    This method insert the temporary CSV files (from config.TEMP_CSV_PATH) into the MySQL database
+    """
 
-# Insert the data
-os.system(cd_to_csv + 'mkdir temp')
-for table in table_list:
-    os.system(cd_to_csv + 'cat {}_*  | tr -d "\r" > ./temp/{}.csv'.format(table, table))
-    os.system(cd_to_csv + 'mysqlimport --fields-escaped-by=\'\\\' --fields-optionally-enclosed-by=\'\"\'  '
-                          '--fields-terminated-by="," --lines-terminated-by="\n"  '
-                          '--verbose  --local -u {} --password={} Merge_Data  ./temp/{}.csv '
-              .format(config.DB_USER_NAME, config.DB_PASSWORD, table))
-os.system(cd_to_csv + 'rm -r temp')
+    cd_to_csv = 'cd {};'.format(config.TEMP_CSV_PATH)
+    table_list = ['Repository',
+                'Conflicting_File',
+                'Conflicting_Region',
+                'Merge_Scenario',
+                'Merge_Replay',
+                'Code_Style_Violation',
+                'Code_Complexity',
+                'Merge_Related_Commit']
+
+    # Create tables
+    os.system('mysql -u {} --password={} < {}Merge_Data.sql'.format(config.DB_USER_NAME
+                                                                    , config.DB_PASSWORD, config.QUERY_PATH))
+
+    # Insert the data
+    os.system(cd_to_csv + 'mkdir temp')
+    for table in table_list:
+        os.system(cd_to_csv + 'cat {}_*  | tr -d "\r" > ./temp/{}.csv'.format(table, table))
+        os.system(cd_to_csv + 'mysqlimport --fields-escaped-by=\'\\\' --fields-optionally-enclosed-by=\'\"\'  '
+                              '--fields-terminated-by="," --lines-terminated-by="\n"  '
+                              '--verbose  --local -u {} --password={} Merge_Data  ./temp/{}.csv '
+                  .format(config.DB_USER_NAME, config.DB_PASSWORD, table))
+    os.system(cd_to_csv + 'rm -r temp')
+
+
+if __name__ == '__main__':
+    insert_csv_to_mysql()
