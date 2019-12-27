3bc79d5ed6ecca25f86f2ebfccd40903138de88a
Moein Owhadi-Kareshk <moein.owhadi@gmail.com>
2019-12-26 22:28:20 -0800

    update the SQL queries

--- queries/commit_density_two_weeks.sql
+++ queries/commit_density_two_weeks.sql
@@ -4,5 +4,4 @@ LEFT JOIN Merge_Data.Merge_Related_Commit Commits
 on merge_scenario.merge_commit_hash = Commits.Merge_Scenario_merge_commit_hash
 AND Commits.merge_commit_parent = {}
 AND TIMESTAMPDIFF(WEEK, merge_scenario.merge_commit_date, Commits.date) < 3
-where merge_scenario.parallel_changed_file_num > 0
 GROUP BY merge_scenario.merge_commit_hash;
